2a2450b96ebf5dc42061a569027b16bbe1eca03f
anas-kirsch <anas83kirsch@gmail.com>
2025-07-20 11:49:26 +0200

    ajout de la fonctionnalité de deconnexion

--- frontend/src/app/header-connected-component/header-connected-component.ts
+++ frontend/src/app/header-connected-component/header-connected-component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { User } from '../interfaces/user';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth/auth-service';
 
 @Component({
   selector: 'app-header-connected-component',
@@ -8,44 +10,65 @@ import { User } from '../interfaces/user';
   styleUrl: './header-connected-component.css'
 })
 export class HeaderConnectedComponent {
-  
-  
-  ngOnInit(){
-    console.log(this.getProfilPicture())
 
+  router: Router = new Router();
+
+  authService = inject(AuthService);
+  isAuthenticated = AuthService.isConnected();
+
+  constructor() {
+    if (this.isAuthenticated) {
+      this.authService.isConnected = true;
+    }
+
+  }
+
+
+  ngOnInit() {
+    // console.log(this.getProfilPicture())
+
+    // a voir 
+  }
+
+
+  getProfilPicture(): User | null {
+    // Récupère tous les cookies
+    const cookies = document.cookie.split('; ');
+    // Trouve le cookie 'auth'
+    const authCookie = cookies.find(row => row.startsWith('auth='));
+    if (!authCookie) return null;
+
+    // Extrait la valeur et la décode
+    const authValue = authCookie.split('=')[1];
+    const decoded = decodeURIComponent(authValue);
+
+    // Parse en objet JS
+    try {
+      const authData = JSON.parse(decoded);
+      // Exemple d'accès à l'image
+      // console.log(authData.imagePath)
+      return authData;
+    } catch (e) {
+      console.error('Erreur de parsing du cookie auth', e);
+      return null;
+    }
   }
 
 
 
 
+  deconnexion() {
+    // Supprime le cookie 'auth'
+    document.cookie = "auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=None; Secure";
+    // Met à jour l'état d'authentification
+    this.isAuthenticated = false;
+    this.authService.isConnected = false;
+    console.log("Cookie 'auth' supprimé");
 
-  getProfilPicture() : User | null{
-  // Récupère tous les cookies
-  const cookies = document.cookie.split('; ');
-  // Trouve le cookie 'auth'
-  const authCookie = cookies.find(row => row.startsWith('auth='));
-  if (!authCookie) return null;
+    // Redirection vers la
 
-  // Extrait la valeur et la décode
-  const authValue = authCookie.split('=')[1];
-  const decoded = decodeURIComponent(authValue);
 
-  // Parse en objet JS
-  try {
-    const authData = JSON.parse(decoded);
-    // Exemple d'accès à l'image
-    // console.log(authData.imagePath)
-    return authData;
-  } catch (e) {
-    console.error('Erreur de parsing du cookie auth', e);
-    return null;
   }
-}
-
-
-deconnexion(){
-}
 
 
 }
-
---
+++ frontend/src/app/profil-component/profil-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-component',
+  imports: [],
+  templateUrl: './profil-component.html',
+  styleUrl: './profil-component.css'
+})
+export class ProfilComponent {
+
+}
